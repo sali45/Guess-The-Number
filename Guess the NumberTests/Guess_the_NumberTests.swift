455ee8fd85caf9cded52593c62ae05fd2239962f
sali45 <sali45@gatech.edu>
2017-02-14 15:31:00 -0500

    Initial Commit

---
+++ Guess the NumberTests/Guess_the_NumberTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guess_the_NumberTests.swift
+//  Guess the NumberTests
+//
+//  Created by Saqib Ali on 2/14/17.
+//  Copyright © 2017 Saqib Ali. All rights reserved.
+//
+
+import XCTest
+@testable import Guess_the_Number
+
+class Guess_the_NumberTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
