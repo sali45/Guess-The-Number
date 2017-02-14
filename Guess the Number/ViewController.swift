71ec3ddd9928732287bf614e38bbd0d965fbda31
sali45 <sali45@gatech.edu>
2017-02-14 15:42:03 -0500

    First commit. Finished first iOS Club tutorial app.

--- Guess the Number/ViewController.swift
+++ Guess the Number/ViewController.swift
@@ -12,9 +12,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generateRandomNumber()
+    }
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        if let guess = guessTextField.text {
+            if let guessInt = Int(guess) {
+                numberOfGuesses = numberOfGuesses + 1
+                validateGuess(guessInt)
+            }
+        }
     }
 
+    let lowerBound = 1
+    let upperBound = 100
+    var numberToGuess: Int!
+    var numberOfGuesses = 0
+    
+    func generateRandomNumber() {
+        numberToGuess = Int(arc4random_uniform(100)) + 1
+    }
+    
+    func validateGuess(_ guess: Int) {
+        if guess < lowerBound || guess > upperBound {
+            showBoundsAlert()
+        } else if guess < numberToGuess {
+            guessLabel.text = "Higher! ⬆️"
+        } else if guess > numberToGuess {
+            guessLabel.text = "Lower! ⬇️"
+        } else {
+            // You win yay!
+            showWinAlert()
+            guessLabel.text = "Guess the Number"
+            numberOfGuesses = 0
+            generateRandomNumber()
+        }
+        guessTextField.text = ""
+    }
+
+    
+    func showBoundsAlert() {
+        let alert = UIAlertController(title: "Hey!", message: "Your guess should be between 1 and 100!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Got it", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showWinAlert() {
+        let alert = UIAlertController(title: "Congrats! 🎉", message: "You won with a total of \(numberOfGuesses) guesses", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Play again", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
